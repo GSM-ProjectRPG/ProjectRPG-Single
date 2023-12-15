5de0851e64a1598bb1965adf2582d4b79740bcc2
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2023-12-15 15:18:21 +0900

    🔥 :: 안쓰는 클래스 삭제
    ActorInfo 삭제

--- Assets/ProjectRPG/Scripts/Actor/ActorInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "스탯", menuName = "Scriptable Object/스탯", order = int.MinValue)]
-public class ActorInfo : ScriptableObject
-{
-    
-}

7889ec80c3421e2a7c56dcd846d6b62893dfcd4f
KGEW0511 <104777650+KGEW0511@users.noreply.github.com>
2023-12-18 13:49:55 +0900

    Revert "Merge branch 'feature-inventory' into develop"
    This reverts commit ba06370f32062c17c52eb5f0af45353221ca9820, reversing
    changes made to 428ae8a5c44b95d2bf998dce56ffb7b7502f61c1.

--- Assets/ProjectRPG/Scripts/Data/Etc.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Etc", menuName = "ScriptableObject/Etc")]
-public class Etc : ItemData
-{
-
-}

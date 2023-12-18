bfc306b41f15cefaa8d62cf9c1a80034fbdac979
LeeKiHwan <s22049@gsm.hs.kr>
2023-12-18 15:30:05 +0900

    ✨  :: 회원가입 기능 추가
    RequestCreateAccount() 메서드 추가
    회원가입 UI 추가

--- Assets/ProjectRPG/Scripts/Menu/MenuManager.cs
+++ Assets/ProjectRPG/Scripts/Menu/MenuManager.cs
@@ -4,21 +4,42 @@ using TMPro;
 public class MenuManager : MonoBehaviour
 {
     [Header("Login")]
-    [SerializeField] private TMP_InputField _userNameInputField;
-    [SerializeField] private TMP_InputField _passwordInputField;
+    [SerializeField] private TMP_InputField _loginUserName;
+    [SerializeField] private TMP_InputField _loginPassword;
+
+    [Space()]
+    [Header("Create Account")]
+    [SerializeField] private TMP_InputField _createAccountUserName;
+    [SerializeField] private TMP_InputField _createAccountPassword;
+    [SerializeField] private TMP_InputField _createAccountConfirmPassword;
 
     public void RequestLogin()
     {
-        string userName = _userNameInputField.text;
-        string password = _passwordInputField.text;
+        string userName = _loginUserName.text;
+        string password = _loginPassword.text;
 
+        Debug.Log("Request Login");
         // 로그인 요청 코드 작성
     }
 
-    public void InitLoginInputField()
+    public void RequestCreateAccount()
     {
-        _userNameInputField.text = "";
-        _passwordInputField.text = "";
+        string userName = _createAccountUserName.text;
+        string password = _createAccountPassword.text;
+        string confirmPassword = _createAccountConfirmPassword.text;
+
+        Debug.Log("Request CA");
+        // 계정생성 요청 코드 작성
+    }
+
+    public void InitLoginAndCreateAccountInputField()
+    {
+        _loginUserName.text = "";
+        _loginPassword.text = "";
+
+        _createAccountUserName.text = "";
+        _createAccountPassword.text = "";
+        _createAccountConfirmPassword.text = "";
     }
 
     public void ExitGame()

bc2a35a489793956ae7db541fd88a24d302ee83d
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2023-12-19 03:10:51 +0900

    ✨ :: ActorManager 작성

---
+++ Assets/ProjectRPG/Scripts/Actor/ActorManager.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorManager : MonoBehaviour
+{
+    public static ActorManager Instance { get; private set; }
+
+    public Action<GameObject> OnRegistedActor;
+    public Action<GameObject> OnDeletedActor;
+    public Action OnRegistedPlayer;
+    public Action OnDeletedPlayer;
+
+    public GameObject Player;
+
+    public List<GameObject> Actors { get; private set; } = new();
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void RegistActor(GameObject actor)
+    {
+        Actors.Add(actor);
+        OnRegistedActor?.Invoke(actor);
+    }
+
+    public void RegistPlayer(GameObject player)
+    {
+        RegistActor(player);
+        Player = player;
+        OnRegistedPlayer?.Invoke();
+    }
+
+    public void DeleteActor(GameObject actor)
+    {
+        if (Actors.Contains(actor))
+        {
+            Actors.Remove(actor);
+            OnDeletedActor?.Invoke(actor);
+            
+            if(actor == Player)
+            {
+                Player = null;
+                OnDeletedPlayer?.Invoke();
+            }
+        }
+    }
+}

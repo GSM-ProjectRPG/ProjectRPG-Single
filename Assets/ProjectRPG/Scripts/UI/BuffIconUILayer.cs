e0a225437086869c6ad2185e60d58ac9cd8d2edb
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2023-12-21 11:28:00 +0900

    ✨ :: 버프 아이콘 구현

--- Assets/ProjectRPG/Scripts/UI/BuffIconUILayer.cs
+++ Assets/ProjectRPG/Scripts/UI/BuffIconUILayer.cs
@@ -6,13 +6,24 @@ public class BuffIconUILayer : MonoBehaviour
 {
     public GameObject BuffIconPrefab;
 
+    private Dictionary<Buff, BuffIconUI> _icons = new();
+
     private void Awake()
     {
         ActorManager.Instance.OnRegistedPlayer += () =>
         {
             BuffSystem buffSystem = ActorManager.Instance.Player.GetComponent<BuffSystem>();
-            
-            
+
+            buffSystem.OnAddedBuff += (buff) =>
+            {
+                BuffIconUI icon = Instantiate(BuffIconPrefab, transform).GetComponent<BuffIconUI>();
+                icon.BindBuff(buff);
+                _icons.Add(buff, icon);
+            };
+            buffSystem.OnRemovedBuff += (buff) =>
+            {
+                Destroy(_icons[buff].gameObject);
+            };
         };
     }
 }

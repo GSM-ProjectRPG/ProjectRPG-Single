45ddf80d086fbc063002bbdd70c41843f1e4708a
didtmdrnjs <105967323+didtmdrnjs@users.noreply.github.com>
2023-12-28 18:16:29 +0900

    ✨스토리 받았을 떄 대화 추가

--- Assets/ProjectRPG/Scripts/NPC/QuestNPC.cs
+++ Assets/ProjectRPG/Scripts/NPC/QuestNPC.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class QuestNPC : MonoBehaviour
 {
     public QuestData QuestData;
+    public GameObject SpeechBubble;
     
     private void Start()
     {
-        GetComponent<InteractableObject>().OnInteracted += (_) => { QuestManager.Instance.RegistQuest(new Quest(QuestData)); };
+        SpeechBubble.SetActive(false);
+        GetComponent<InteractableObject>().OnInteracted += (_) => { 
+            QuestManager.Instance.RegistQuest(new Quest(QuestData));
+            ShowStory();
+        };
+    }
+
+    public void ShowStory()
+    {
+        SpeechBubble.SetActive(true);
+        StartCoroutine(SpeechBubble.GetComponent<StoryProcessor>().Story(QuestData.Story));
     }
 }

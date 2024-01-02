d9f5e3db916bfc5e407eed4bf691502d26ebc855
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-01-03 02:08:36 +0900

    ✨ :: 리소스 매니저 작성

---
+++ Assets/ProjectRPG/Scripts/ResourceBindingData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "리소스 바인딩 데이터", menuName = "Scriptable Object/리소스 바인딩 데이터", order = int.MinValue)]
+public class ResourceBindingData : ScriptableObject
+{
+    
+}

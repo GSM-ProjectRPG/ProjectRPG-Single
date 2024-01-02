7cc5d7f7a92cd43075abd181141649e17a0220f2
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-01-03 01:08:44 +0900

    ✨ :: 버프 아이콘에 지속시간 묘사

--- Assets/ProjectRPG/Scripts/Actor/Buff/TimerBuff.cs
+++ Assets/ProjectRPG/Scripts/Actor/Buff/TimerBuff.cs
@@ -2,19 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class TimerBuff<T> : Buff<T> where T : TimerBuff<T>
+public interface TimerBuff
+{
+    public float RemainingTime { get; }
+    public float RemainingTimeRate { get; }
+}
+
+public abstract class TimerBuff<T> : Buff<T>, TimerBuff where T : TimerBuff<T>
 {
     public float RemainingTime { get; protected set; }
+    public float RemainingTimeRate => 1 - RemainingTime / _originTime;
+
+    private float _originTime;
 
     public TimerBuff(BuffData buffData, float remainingTime) : base(buffData)
     {
+        _originTime = remainingTime;
         RemainingTime = remainingTime;
     }
 
     public override void OnUpdate(BuffSystem manager)
     {
         RemainingTime -= Time.deltaTime;
-        if(RemainingTime <= 0)
+        if (RemainingTime <= 0)
         {
             manager.RemoveBuff<T>();
         }
--- Assets/ProjectRPG/Scripts/UI/BuffIconUI.cs
+++ Assets/ProjectRPG/Scripts/UI/BuffIconUI.cs
@@ -8,10 +8,19 @@ public class BuffIconUI : MonoBehaviour, IBuffIconUI<Buff>
     public Image BuffImage;
     public Image Cover;
 
+    private TimerBuff _timerBuff;
+
     public virtual void BindBuff(Buff buff)
     {
         BuffImage.sprite = buff.Sprite;
         Cover.sprite = buff.Sprite;
         Cover.fillAmount = 0;
+
+        _timerBuff = buff as TimerBuff;
+    }
+
+    private void Update()
+    {
+        Cover.fillAmount = _timerBuff.RemainingTimeRate;
     }
 }
